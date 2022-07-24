1910bf55019fbbfb0676acf0de0e3378fc30d854
Jaspreet85 <109520285+Jaspreet85@users.noreply.github.com>
2022-07-24 15:36:42 +0530

    Contain Main Activity and xml of Login

---
+++ app/src/main/java/com/JaspreetKaur/login/ForgetPassword.kt
@@ -0,0 +1,14 @@
+package com.JaspreetKaur.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgetPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+
+
+    }
+}
--- app/src/main/java/com/JaspreetKaur/login/MainActivity.kt
+++ app/src/main/java/com/JaspreetKaur/login/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.JaspreetKaur.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etMail: EditText
+    lateinit var etPassword: EditText
+    lateinit var tvForgotPassword: TextView
+    lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etMail = findViewById(R.id.etMail)
+        etPassword = findViewById(R.id.etPassword)
+        tvForgotPassword = findViewById(R.id.tvForgotPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+
+
+
+           tvForgotPassword.setOnClickListener {
+
+               var intent = Intent(this, ForgetPassword::class.java)
+               startActivity(intent)
+           }
+
+            btnLogin.setOnClickListener {
+                System.out.println("Login Clicked")
+
+            var email = etMail.text.toString()
+            var password = etPassword.text.toString()
+            System.out.println("email $email")
+            if(email.isNullOrEmpty()){
+                etMail.error = resources.getString(R.string.please_enter_email)
+                etMail.requestFocus()
+
+             }
+            else if (password.isNullOrEmpty()){
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+             }
+            else if (password.length<6) {
+                etPassword.error = resources.getString(R.string.password_should_be_greater_than_6)
+                etPassword.requestFocus()
+            }
+            else {
+                Toast.makeText(this,resources.getString(R.string.login_successfully), Toast.LENGTH_LONG).show()
+                var intent = Intent(this, MainScreenActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
+
